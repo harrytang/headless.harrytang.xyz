c646c96452eb28acfd30fc7b6c452d16f5cd8055
harrytang <harry@powerkernel.com>
2022-03-13 22:37:10 +0200

    fix: db config

--- app/config/database.js
+++ app/config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USER", "strapi"),
-      password: env("MYSQL_PASSWORD", "strapi"),
+      password: env("DATABASE_PASS", "strapi"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
