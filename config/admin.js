de4819ffe1b6db800b23ba0332db426ae0cd54a6
harrytang <harry@powerkernel.com>
2022-11-09 20:51:31 +0200

    chore: remove deprecated API_TOKEN_SALT

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '2d1d3edb6b0f4f8eeb72815f0aeb4f60'),
+    secret: env("ADMIN_JWT_SECRET", "2d1d3edb6b0f4f8eeb72815f0aeb4f60"),
   },
+  apiToken: { salt: env("API_TOKEN_SALT") },
 });
